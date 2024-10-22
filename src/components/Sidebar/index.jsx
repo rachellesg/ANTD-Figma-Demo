9c206038f7a89e5f81b8ea2434d1a1e155c08810
Rachelle <56790705+rachellesg@users.noreply.github.com>
2024-10-22 16:34:05 +0800

    Update index.jsx

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -14,83 +14,76 @@ import React from "react";
 const { Sider } = Layout;
 
 const Sidebar = () => {
+  console.log("load");
   return (
-      <Sider width={300} theme="dark">
-        <div style={{ padding: "16px", textAlign: "center" }}>
-          <img
-            src="https://via.placeholder.com/54"
-            alt="Logo"
-            style={{ width: 54, height: 50 }}
-          />
-          <div
-            style={{ color: "#f6f6f6", fontWeight: "bold", fontSize: "20px" }}
-          >
-            ISMP
-          </div>
-          <div style={{ color: "#f6f6f6", fontSize: "15px" }}>
-            Access Control System
-          </div>
+    <Sider width={300} theme="dark">
+      <div style={{ padding: "16px", textAlign: "center" }}>
+        <img
+          src="https://via.placeholder.com/54"
+          alt="Logo"
+          style={{ width: 54, height: 50 }}
+        />
+        <div style={{ color: "#f6f6f6", fontWeight: "bold", fontSize: "20px" }}>
+          ISMP
         </div>
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1" icon={<HomeOutlined />}>
-            Home
-          </Menu.Item>
-          <Menu.Item key="2" icon={<FileTextOutlined />}>
-            Whitelist Management
-          </Menu.Item>
-          <Menu.SubMenu
-            key="sub1"
-            icon={<UserOutlined />}
-            title="Blacklist Management"
-          >
-            <Menu.Item key="3">Option 1</Menu.Item>
-            <Menu.Item key="4">Option 2</Menu.Item>
-          </Menu.SubMenu>
-          <Menu.SubMenu
-            key="sub2"
-            icon={<UserOutlined />}
-            title="Physical User Management"
-          >
-            <Menu.Item key="5">Option 1</Menu.Item>
-            <Menu.Item key="6">Option 2</Menu.Item>
-          </Menu.SubMenu>
-          <Menu.SubMenu
-            key="sub3"
-            icon={<BarChartOutlined />}
-            title="Analytics Management"
-          >
-            <Menu.Item key="7">Option 1</Menu.Item>
-            <Menu.Item key="8">Option 2</Menu.Item>
-          </Menu.SubMenu>
-          <Menu.SubMenu
-            key="sub4"
-            icon={<FileDoneOutlined />}
-            title="Report Management"
-          >
-            <Menu.Item key="9">Option 1</Menu.Item>
-            <Menu.Item key="10">Option 2</Menu.Item>
-          </Menu.SubMenu>
-          <Menu.SubMenu
-            key="sub5"
-            icon={<ToolOutlined />}
-            title="Device Management"
-          >
-            <Menu.Item key="11">Option 1</Menu.Item>
-            <Menu.Item key="12">Option 2</Menu.Item>
-          </Menu.SubMenu>
-          <Menu.SubMenu
-            key="sub6"
-            icon={<MonitorOutlined />}
-            title="Monitoring Tools"
-          >
-            <Menu.Item key="13">Option 1</Menu.Item>
-            <Menu.Item key="14">Option 2</Menu.Item>
-          </Menu.SubMenu>
-        </Menu>
-        <div style={{ padding: "16px", textAlign: "center" }}>
-          <MenuFoldOutlined style={{ fontSize: "24px", color: "#f6f6f6" }} />
+        <div style={{ color: "#f6f6f6", fontSize: "15px" }}>
+          Access Control System
         </div>
-      </Sider>
+      </div>
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+        <Menu.Item key="1" icon={<HomeOutlined />}>
+          Home
+        </Menu.Item>
+        <Menu.Item key="2" icon={<FileTextOutlined />}>
+          Whitelist Management
+        </Menu.Item>
+        <Menu.SubMenu
+          key="sub1"
+          icon={<UserOutlined />}
+          title="Blacklist Management">
+          <Menu.Item key="3">Option 1</Menu.Item>
+          <Menu.Item key="4">Option 2</Menu.Item>
+        </Menu.SubMenu>
+        <Menu.SubMenu
+          key="sub2"
+          icon={<UserOutlined />}
+          title="Physical User Management">
+          <Menu.Item key="5">Option 1</Menu.Item>
+          <Menu.Item key="6">Option 2</Menu.Item>
+        </Menu.SubMenu>
+        <Menu.SubMenu
+          key="sub3"
+          icon={<BarChartOutlined />}
+          title="Analytics Management">
+          <Menu.Item key="7">Option 1</Menu.Item>
+          <Menu.Item key="8">Option 2</Menu.Item>
+        </Menu.SubMenu>
+        <Menu.SubMenu
+          key="sub4"
+          icon={<FileDoneOutlined />}
+          title="Report Management">
+          <Menu.Item key="9">Option 1</Menu.Item>
+          <Menu.Item key="10">Option 2</Menu.Item>
+        </Menu.SubMenu>
+        <Menu.SubMenu
+          key="sub5"
+          icon={<ToolOutlined />}
+          title="Device Management">
+          <Menu.Item key="11">Option 1</Menu.Item>
+          <Menu.Item key="12">Option 2</Menu.Item>
+        </Menu.SubMenu>
+        <Menu.SubMenu
+          key="sub6"
+          icon={<MonitorOutlined />}
+          title="Monitoring Tools">
+          <Menu.Item key="13">Option 1</Menu.Item>
+          <Menu.Item key="14">Option 2</Menu.Item>
+        </Menu.SubMenu>
+      </Menu>
+      <div style={{ padding: "16px", textAlign: "center" }}>
+        <MenuFoldOutlined style={{ fontSize: "24px", color: "#f6f6f6" }} />
+      </div>
+    </Sider>
   );
 };
 
